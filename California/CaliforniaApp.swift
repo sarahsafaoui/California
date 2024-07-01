47460e09bb270d6a54e083b6eeae81c5af8f70c2
Sarah <sarsafaoui@gmail.com>
2024-06-30 20:29:13 -0700

    Initial Commit

---
+++ California/CaliforniaApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaliforniaApp.swift
+//  California
+//
+//  Created by sarah safaoui on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct CaliforniaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
